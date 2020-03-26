0f0dcc9fbea004ad0b2025bf5a18b30a6be8da63
Fariha Mahjabin <60178140+Mahjabin0@users.noreply.github.com>
2020-03-26 16:42:52 +0600

    Add files via upload

---
+++ Dob.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<script>
+    function myFun(form) {
+        var a = document.getElementById("a1").value;
+        
+        if ( a=="") {
+           document.getElementById("Message").innerHTML="Enter The  date";
+				return false;
+        }
+        return true;
+        header("location:Dob.php");
+    }
+</script>
+</head>
+
+
+<body>
+Date Of Birth:<br>
+						
+							<input id="a1" type="Date" name="rdate">
+							<!-- <input type="text" size="2" name="date">/
+							<input type="text" size="2" name="month">/
+							<input type="text" size="2" name="year"> -->
+							<br>
+<button onclick="myFun()" id="btn1">Submit</button>
+<span id="Message"></span><br>
+</body>
+</html>
